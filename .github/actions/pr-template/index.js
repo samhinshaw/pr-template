35e6d9c290e0dc37ffdf023134a6e6b4e727f435
Sam Hinshaw <samuel.hinshaw@abcellera.com>
2021-03-05 15:51:06 -0800

    push up workflow

--- .github/actions/pr-template/index.js
+++ .github/actions/pr-template/index.js
@@ -4,27 +4,37 @@ const github = require('@actions/github');
 const axios = require('axios')
 const fs = require('fs')
 
-try {
-  const token = core.getInput('github-token');
+const REPO_OWNER = 'samhinshaw'
+const REPO_NAME = 'pr-template'
 
-  // const requestUrl = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls/${PULL_NUMBER}`
+try {
+  const token = core.getInput('github_token');
+  const prNumber = core.getInput('pr_number');
+  const branchName = core.getInput('branch_name');
+  console.log({
+    token,
+    prNumber,
+    branchName,
+  })
+
+  const requestUrl = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls/${prNumber}`
 
   // template out PR body
-  const template = fs.readFileSync('./templates/test.md', {encoding: 'utf-8'})
+  const template = fs.readFileSync('./templates/test.md', { encoding: 'utf-8' })
   console.log(template)
 
-  // const res = axios.request({
-  //   url: requestUrl,
-  //   method: 'patch',
-  //   headers: {
-  //     'accept': 'application/vnd.github.v3+json',
-  //     'Authorization': `token ${TOKEN}`
-  //   },
-  //   data: {
-  //     body: template,
-  //   },
-  // })
-  // res.then(() => console.log('done!'))
+  axios.request({
+    url: requestUrl,
+    method: 'patch',
+    headers: {
+      'accept': 'application/vnd.github.v3+json',
+      'Authorization': `token ${TOKEN}`
+    },
+    data: {
+      body: template,
+    },
+  })
+  .then(() => console.log('done!'))
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
