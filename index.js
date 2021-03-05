7ccc13807a13e2621e27ec0a5ff11589e8ed62ce
Sam Hinshaw <samuel.hinshaw@abcellera.com>
2021-03-05 15:29:12 -0800

    scaffold for request

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const axios = require('axios')
+const fs = require('fs')
+
 try {
+  const token = core.getInput('github-token');
+
+  // const requestUrl = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls/${PULL_NUMBER}`
+
+  // template out PR body
+  const template = fs.readFileSync('./test.md', {encoding: 'utf-8'})
+  console.log(template)
+
+  // const res = axios.request({
+  //   url: requestUrl,
+  //   method: 'patch',
+  //   headers: {
+  //     'accept': 'application/vnd.github.v3+json',
+  //     'Authorization': `token ${TOKEN}`
+  //   },
+  //   data: {
+  //     body: template,
+  //   },
+  // })
+  // res.then(() => console.log('done!'))
+
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
