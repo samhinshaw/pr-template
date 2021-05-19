a17f3990afb97a1656a7efef3e8cf1b745140c4b
Sam Hinshaw <samuel.hinshaw@abcellera.com>
2021-05-19 15:49:58 -0700

    fill out PR template

--- .github/actions/pr-template/index.js
+++ .github/actions/pr-template/index.js
@@ -1,49 +1,76 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const axios = require('axios')
-const fs = require('fs')
+const axios = require('axios');
+const fs = require('fs');
 
-const REPO_OWNER = 'samhinshaw'
-const REPO_NAME = 'pr-template'
-
-try {
-  const token = core.getInput('github_token');
-  const prNumber = core.getInput('pr_number');
-  const branchName = core.getInput('branch_name');
-  console.log({
-    token,
-    prNumber,
-    branchName,
-  })
-
-  const requestUrl = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls/${prNumber}`
-
-  // template out PR body
-  const template = fs.readFileSync('./templates/test.md', { encoding: 'utf-8' })
-  console.log(template)
-
-  axios.request({
-    url: requestUrl,
-    method: 'patch',
-    headers: {
-      'accept': 'application/vnd.github.v3+json',
-      'Authorization': `token ${TOKEN}`
-    },
-    data: {
-      body: template,
-    },
-  })
-  .then(() => console.log('done!'))
-
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+/**
+ * Extract a Celium Software jira ticket id from a branch name.
+ *
+ * @param {string} branchName
+ * @returns a formatted string to link to the jira ticket
+ */
+function getJiraTicketId(branchName) {
+  // get the jira ticket number from the branch, if possible
+  const match = branchName.match(/CS-[0-9]{4,}/i);
+  if (match) {
+    const ticketId = match[0];
+    return ticketId;
+  }
 }
+
+/**
+ * Format a ticketId into a markdown link.
+ *
+ * @param {string} ticketId
+ * @returns
+ */
+function formatJiraTicketLink(ticketId) {
+  if (ticketId) {
+    const jiraTicketLink = `[ticket](https://celium.atlassian.net/browse/${ticketId})`;
+    return jiraTicketLink;
+  }
+  return '_No ticket found._';
+}
+
+/**
+ * Fill out a template with the provided pattern/replacement pairs.
+ *
+ * @param {string} emptyTemplate
+ * @param {[RegExp, string][]} templateReplacements
+ * @returns
+ */
+function fillOutPrTemplate(emptyTemplate, templateReplacements) {
+  return templateReplacements.reduce(
+    (template, [pattern, replacement]) =>
+      template.replace(pattern, replacement),
+    emptyTemplate,
+  );
+}
+
+function main() {
+  try {
+    const branchName = core.getInput('branch_name');
+    const author = core.getInput('author');
+
+    const jiraTicketLink = formatJiraTicketLink(getJiraTicketId(branchName));
+
+    const templateReplacements = [
+      [/PR_AUTHOR/, author],
+      [/JIRA_TICKET_LINK/, jiraTicketLink],
+    ];
+
+    // template out PR body
+    const TEMPLATE_PATH = './templates/test.md';
+    const template = fs.readFileSync(TEMPLATE_PATH, {
+      encoding: 'utf-8',
+    });
+    const prDescription = fillOutPrTemplate(template, templateReplacements);
+
+    core.setOutput('pr_description', prDescription);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
